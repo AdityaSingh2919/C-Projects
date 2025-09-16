0167a88cc937dcf9f94c75f6e9a41faeeed4c921
Aditya Singh <adityasinghrajput1474@gmail.com>
2025-09-16 15:43:12 +0530

    Create CheckingEvenOrOdd.cpp

---
+++ CheckingEvenOrOdd.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+//checking wheather the number is even or odd without using modulus 
+int num;
+cout<<"Enter the number :";
+cin>>num;
+if ((num / 2) * 2 == num)
+cout <<"Even";
+else
+cout <<"odd";
+
+
+
+    return 0;
+}
