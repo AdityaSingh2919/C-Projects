0481664405fa75e40eaa491f6816bbe5ed4eeddc
Aditya Singh <adityasinghrajput1474@gmail.com>
2026-07-23 15:30:19 +0530

    Create sort by parity.java

---
+++ sort by parity.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        // Arrays.sort(nums);
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] % 2== 0){
+                int c=nums[i];
+                nums[i]=nums[j];
+                nums[j]=c;
+                j++;
+
+            }
+        }
+            return nums;
+    }
+}
