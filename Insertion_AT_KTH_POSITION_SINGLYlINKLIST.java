fe941a65ba7b0b41655105b751d6486013392f74
Aditya Singh <adityasinghrajput1474@gmail.com>
2026-08-04 22:11:41 +0530

    Implement insertion at Kth position in linked list

---
+++ Insertion_AT_KTH_POSITION_SINGLYlINKLIST.java
@@ -0,0 +1,31 @@
+/* Structure of Linked List Node
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+*/
+class Solution {
+    public Node insertPos(Node head, int pos, int val) {
+        // code here
+        Node temp=head;
+        Node newNode=new Node(val);
+    
+        if (pos == 1) {
+            newNode.next = head;
+            return newNode;
+        }
+        for(int i=1;i<pos-1;i++){
+            temp=temp.next;
+        }
+        newNode.next=temp.next;
+        
+        temp.next=newNode;
+        
+        return head;
+    }
+}
